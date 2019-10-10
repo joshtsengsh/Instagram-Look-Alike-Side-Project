5cc4fb5d75b46e176f3436990baef8ec81d24796
Joshua Tseng Shi-Hao <joshuatseng.2017@sis.smu.edu.sg>
2019-10-10 18:27:29 +0800

    Created file structure
    - Basic page files for index, login, footer, navigation, and newsletter.
    - Created directory structure to use on the project.

---
+++ app/newsletter.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Sign Up for Our Newsletter</h1>
+
+<form id="newsletter" method="post" action="">
+
+<input type="email" name="email">
+
+<input type="submit" value="Sign Up">
+
+</form>
+
+
+</body>
+</html>
